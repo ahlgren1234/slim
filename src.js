c31337adfc66741dad5d68448b7bfaf09575d9ac
Peter Ahlgren <ahlgren1234@gmail.com>
2018-01-24 15:06:31 +0100

    Added examples and fixed a few bugs

--- src.js
+++ src.js
@@ -1,28 +1,28 @@
 //
 //////////////// MODAL
 //
+if ($(".modal")[0]) {
+    // Get modal element
+    var modal = document.getElementById('modal');
+    // Get open modal button
+    var modalBtn = document.getElementById('modalBtn');
+    // Get close button
+    var modalCloseBtn = document.getElementsByClassName('modalCloseBtn')[0];
+    // Listen for click on button
+    modalBtn.addEventListener('click', openModal);
+    // Listen for modal close button click
+    modalCloseBtn.addEventListener('click', closeModal);
 
-// Get modal element
-var modal = document.getElementById('modal');
-// Get open modal button
-var modalBtn = document.getElementById('modalBtn');
-// Get close button
-var modalCloseBtn = document.getElementsByClassName('modalCloseBtn')[0];
-// Listen for click on button
-modalBtn.addEventListener('click', openModal);
-// Listen for modal close button click
-modalCloseBtn.addEventListener('click', closeModal);
+    // Function to open modal
+    function openModal() {
+        modal.style.display = 'block';
+    }
 
-// Function to open modal
-function openModal() {
-    modal.style.display = 'block';
+    // Close the modal
+    function closeModal() {
+        modal.style.display = 'none';
+    }
 }
-
-// Close the modal
-function closeModal() {
-    modal.style.display = 'none';
-}
-
 //
 ///////// END MODAL
 //
@@ -31,11 +31,12 @@ function closeModal() {
 // ALERT BOX
 //
 
-// Hide the alert box when clicked on the close button.
-$('.alert-close').click(function() {
-    $(this).closest('.alert-closable').hide("slow");
-})
-
+if($(".alert-close")[0]) {
+    // Hide the alert box when clicked on the close button.
+    $('.alert-close').click(function() {
+        $(this).closest('.alert-closable').hide("slow");
+    });
+}
 
 //
 // ALERT BOX END 
